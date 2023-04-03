036370e236724059a4f6ea79f71461e338e60c0b
Portugapt <monteiro.joao.ps@gmail.com>
2023-04-03 08:39:25 +0100

    remove project from main

--- src/app/checkout.guard.ts
+++
@@ -1,9 +0,0 @@
-import { CanDeactivateFn } from '@angular/router';
-import { CartComponent } from './cart/cart.component';
-
-export const checkoutGuard: CanDeactivateFn<CartComponent> = () => {
-  const confirmation = confirm(
-    'You have pending items in your cart. Do you want to continue?'
-  );
-  return confirmation;
-};
--- src/app/products/price-range.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { priceRangeValidator } from './price-range.directive';
-
-describe('PriceRangeDirective', () => {
-  it('should create an instance', () => {
-    const directive = priceRangeValidator();
-    expect(directive).toBeTruthy();
-  });
-});
